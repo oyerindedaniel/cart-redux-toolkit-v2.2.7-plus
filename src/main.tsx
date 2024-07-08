e8f059f54e8bce6c52550d133fc3a1a8a784947f
oyerindedaniel <oyerinde.daniel@yahoo.com>
2024-07-09 00:33:09 +0100

    changed font path

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./reset.scss";
 
-import "./assets/styles/fonts.module.scss";
 import "./index.scss";
 import router from "./routes";
+import "./styles/_fonts.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
